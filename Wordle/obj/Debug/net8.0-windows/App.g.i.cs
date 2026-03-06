7d4f0fc228171cd9d131fa551aa11c2c038a866f
TakacsLaszlo01 <takacs.laszlo.istvan.01.moricz@gmail.com>
2026-03-06 08:51:22 +0100

    Update Wordle WPF

--- Wordle/obj/Debug/net8.0-windows/App.g.i.cs
+++ Wordle/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Wordle {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Wordle {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
         public static void Main() {
             Wordle.App app = new Wordle.App();
             app.InitializeComponent();
